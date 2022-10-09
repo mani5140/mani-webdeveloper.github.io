36dc48b0c6eccb490363ecec5ec5abbb3f8de999
mani5140 <srimanikantagarapati12@gmail.com>
2022-10-09 22:52:38 +0530

    My Portfolio website

---
+++ script.js
@@ -0,0 +1,21 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var side = document.getElementById("sidemenu");
+function openmenu(){
+    side.style.right = "0"; 
+}
+function closemenu(){
+    side.style.right = "-170px";
+}
